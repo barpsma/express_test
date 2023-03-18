61927caf889a4c43db27695da38040ba5957b710
akbar <barmuhammad44@gmail.com>
2023-03-18 13:35:30 +0700

    ci/cd pipeline [github actions]

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ const port = 3000
 app.get('/', (req, res) => {
     res.send("halo guys")
 })
+app.get('/user', (req, res) => {
+    res.send("halo guys ini halaman user ya")
+})
 
 app.listen(port, () => console.log(`listening port ${port}`))
